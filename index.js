999250c3010ebb11dce4f942cb2ea59fb6ec1da8
Nhan Phan <nphanacc@gmail.com>
2022-07-01 08:12:25 -0700

    wip-hamburger menu

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+	console.log("you are inside myFunction");
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
 // carousel by next and previous buttons
 const buttons = document.querySelectorAll("[data-carousel-button]");
 console.log("buttons: ", buttons);
