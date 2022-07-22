f7fdb6445add0e64b919fce127102c63f0b1e31a
Nhan Phan <nphan@bluebeam.com>
2022-07-22 06:39:42 -0700

    add example of accessibility menu button

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-	console.log("you are inside myFunction");
-	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
+  console.log("you are inside myFunction");
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
 // carousel by next and previous buttons
@@ -14,46 +14,52 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 console.log("buttons: ", buttons);
 
 buttons.forEach((button) => {
-	button.addEventListener("click", () => {
-		const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-		const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
 
-		const activeSlide = slides.querySelector("[data-active]");
-		let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-		console.log(newIndex);
-		if (newIndex < 0) newIndex = slides.children.length - 1;
-		if (newIndex >= slides.children.length) newIndex = 0;
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    console.log(newIndex);
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
 
-		slides.children[newIndex].dataset.active = true;
-		delete activeSlide.dataset.active;
-	});
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
 });
 
 // carousel by indicator
 document.querySelectorAll(".carouseel").forEach((carouseel) => {
-	const items = carouseel.querySelectorAll(".carouseel__item");
-	const buttonsHtml = Array.from(items, () => {
-		return `<button class="carouseel__button"></button>`;
-	});
+  const items = carouseel.querySelectorAll(".carouseel__item");
+  const buttonsHtml = Array.from(items, () => {
+    return `<button class="carouseel__button"></button>`;
+  });
 
-	carouseel.insertAdjacentHTML(
-		"beforeend",
-		`<span class="carouseel__nav">
+  carouseel.insertAdjacentHTML(
+    "beforeend",
+    `<span class="carouseel__nav">
 	    ${buttonsHtml.join(" ")}
 	  </span>
     `
-	);
+  );
 
-	const buttons = carouseel.querySelectorAll(".carouseel__button");
+  const buttons = carouseel.querySelectorAll(".carouseel__button");
 
-	buttons.forEach((button, i) => {
-		button.addEventListener("click", () => {
-			//un-select all the items
-			items.forEach((item) => item.classList.remove("carouseel__item--selected"));
-			buttons.forEach((button) => button.classList.remove("carouseel__button--selected"));
+  buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      //un-select all the items
+      items.forEach((item) =>
+        item.classList.remove("carouseel__item--selected")
+      );
+      buttons.forEach((button) =>
+        button.classList.remove("carouseel__button--selected")
+      );
 
-			items[i].classList.add("carouseel__item--selected");
-			button.classList.add("carouseel__button--selected");
-		});
-	});
+      items[i].classList.add("carouseel__item--selected");
+      button.classList.add("carouseel__button--selected");
+    });
+  });
 });
